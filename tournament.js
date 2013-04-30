b93c5eb137ab32bc78665fef7f844a2f04d037fd
Bill <billvaleriote@gmail.com>
2013-04-30 00:32:23 -0400

    Only allow 8 players to be added.

--- tournament.js
+++ tournament.js
@@ -2,10 +2,9 @@ function TournamentCtrl ($scope) {
 	$scope.players = [];
 
 	$scope.addPlayer = function () {
-		if ($scope.newPlayer) {
+		if ($scope.newPlayer && $scope.players.length < 8 ) {
 			$scope.players.push({text:$scope.newPlayer});
 			$scope.newPlayer = '';
 		}
 	};
-
 }
